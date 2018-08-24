79fc3b7f38a06ef84c5d229e01ba98cd7d71fbe5
Juan <jdelsolar@gmail.com>
2018-08-24 16:03:15 -0300

    se visualiza su foto

--- src/app/pages/abono/abono.component.ts
+++ src/app/pages/abono/abono.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { UsuarioService } from '../../services/usuario.service';
-import { TransferenciasService } from '../../services/transferencias.service';
+import { Component, OnInit } from "@angular/core";
+import { UsuarioService } from "../../services/usuario.service";
+import { TransferenciasService } from "../../services/transferencias.service";
 
 @Component({
-  selector: 'app-abono',
-  templateUrl: './abono.component.html',
-  styleUrls: ['./abono.component.css']
+  selector: "app-abono",
+  templateUrl: "./abono.component.html",
+  styleUrls: ["./abono.component.css"]
 })
 export class AbonoComponent implements OnInit {
-
-  pesos: number = 0 ;
+  imagen: string = null;
+  pesos: number = 0;
   tasa: number = 371;
-  bolivares:number = 0;
-  gastos:number = 1000;
+  bolivares: number = 0;
+  gastos: number = 1000;
 
   transferi: boolean = false;
 
-  constructor( public _usuario:UsuarioService, public _transferencias: TransferenciasService ) { }
+  constructor(
+    public _usuario: UsuarioService,
+    public _transferencias: TransferenciasService
+  ) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   calcular() {
-
-    this.bolivares =  this.pesos * this.tasa + this.gastos;
-    
+    this.bolivares = this.pesos * this.tasa + this.gastos;
   }
 
+  seleccionarImagen(img: any) {
+    if (img) {
+      if (img.type.indexOf("image") >= 0) {
+        let reader = new FileReader();
+        let urlTemp = reader.readAsDataURL(img);
+
+        reader.onloadend = () => {
+          this.imagen = reader.result.toString();
+        };
+      }
+    }
+  }
 }
