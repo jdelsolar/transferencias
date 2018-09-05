fc8235479d5eacefa06afd0b25dbfac24241c4a4
Juan <jdelsolar@gmail.com>
2018-09-05 12:47:27 -0300

    se agraga pagina como

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
 // export const URL_SERVICIOS = "http://localhost/transferencias_rest/index.php";
-export const URL_SERVICIOS = "http://123depositos.com/transferencias_rest/index.php";
+export const URL_SERVICIOS = "http://www.123depositos.com/transferencias_rest/index.php";
