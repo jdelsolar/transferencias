15215b4cbb65701adf44b58e07172057327f6950
juan <jdelsolar@gmail.com>
2018-09-09 19:49:34 -0300

    agrega formulario de contacto

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ContactoService } from '../../services/contacto.service';
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor( public _contacto:ContactoService ) { }
 
   ngOnInit() {
+
+    this.forma = new FormGroup({
+      nombre: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      asunto: new FormControl('', Validators.required),
+      nacionalidad: new FormControl('', Validators.required),
+      mensaje: new FormControl('', Validators.required)
+    });
+
+  }
+
+  enviar() {
+    if (this.forma.valid){
+
+      this._contacto.mensaje = {
+        nombre: this.forma.get('nombre').value,
+        email: this.forma.get('email').value,
+        asunto: this.forma.get('asunto').value,
+        nacionalidad: this.forma.get('nacionalidad').value,
+        mensaje: this.forma.get('mensaje').value
+      };
+
+      this._contacto.enviar();
+
+    }
   }
 
 }
